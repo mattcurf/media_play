552da3a3b23ef5a5bf78d69b4fa17fd918bc8210
Matt Curfman <matt@tecnovelty.com>
2020-09-01 15:07:22 -0700

    Add method using apt packages

--- msdk_apt/run.sh
+++ msdk_apt/run.sh
@@ -5,6 +5,7 @@
 # $ wget https://download.blender.org/peach/bigbuckbunny_movies/big_buck_bunny_1080p_h264.mov
 # $ ffmpeg -i big_buck_bunny_1080p_h264.mov -an -vcodec copy -bsf h264_mp4toannexb -f h264 big_buck_bunny_1080p_h264.264
 
-docker build -t msdk .
-docker run -it --rm --privileged --net=host -e LD_LIBRARY_PATH=/opt/intel/mediasdk/lib -e DISPLAY=$DISPLAY -v $HOME:/home msdk /opt/intel/mediasdk/share/mfx/samples/sample_decode h264 -i /home/big_buck_bunny_1080p_h264.264 -r
+docker build -t msdk-apt .
+docker run -it --rm --privileged --net=host -e LD_LIBRARY_PATH=/opt/intel/mediasdk/lib -e DISPLAY=$DISPLAY -v $HOME:/home msdk-apt /usr/share/mfx/samples/sample_decode h264 -i /home/big_buck_bunny_1080p_h264.264 -r
+
 
