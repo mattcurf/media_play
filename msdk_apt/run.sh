d80b9e2e1441fc04e9f9d3303d9491703db1762a
Matt Curfman <matt@tecnovelty.com>
2020-09-02 09:10:47 -0700

    Ensure msdk_src builds with X11_DRI3 to match apt packages

--- msdk_apt/run.sh
+++ msdk_apt/run.sh
@@ -6,6 +6,6 @@
 # $ ffmpeg -i big_buck_bunny_1080p_h264.mov -an -vcodec copy -bsf h264_mp4toannexb -f h264 big_buck_bunny_1080p_h264.264
 
 docker build -t msdk-apt .
-docker run -it --rm --privileged --net=host -e LD_LIBRARY_PATH=/opt/intel/mediasdk/lib -e DISPLAY=$DISPLAY -v $HOME:/home msdk-apt /usr/share/mfx/samples/sample_decode h264 -i /home/big_buck_bunny_1080p_h264.264 -r
+docker run -it --rm --privileged --net=host -e DISPLAY=$DISPLAY -v $HOME:/home msdk-apt /usr/share/mfx/samples/sample_decode h264 -i /home/big_buck_bunny_1080p_h264.264 -r -rgb4
 
 
