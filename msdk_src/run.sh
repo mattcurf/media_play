39a3c57c6f834734689255e3d13abc7c16d6986d
Matt Curfman <matt@tecnovelty.com>
2021-01-12 19:55:23 -0800

    Seperated dev from runtime containers

--- msdk_src/run.sh
+++ msdk_src/run.sh
@@ -6,5 +6,6 @@
 # $ ffmpeg -i big_buck_bunny_1080p_h264.mov -an -vcodec copy -bsf h264_mp4toannexb -f h264 big_buck_bunny_1080p_h264.264
 
 docker build -t msdk .
-docker run -it --rm --privileged --net=host -e LD_LIBRARY_PATH=/opt/intel/mediasdk/lib -e DISPLAY=$DISPLAY -v $HOME:/home msdk /opt/intel/mediasdk/share/mfx/samples/sample_decode h264 -i /home/big_buck_bunny_1080p_h264.264 -r -rgb4
+docker build -t msdk-runtime -f Dockerfile.runtime .
+docker run -it --rm --privileged --net=host -e LD_LIBRARY_PATH=/opt/intel/mediasdk/lib -e DISPLAY=$DISPLAY -v $HOME:/home msdk-runtime /opt/intel/mediasdk/share/mfx/samples/sample_decode h264 -i /home/big_buck_bunny_1080p_h264.264 -r -rgb4
 
