a80c5c315e40db0a8223f979fd9c746ed5c0c2ec
Matt Curfman <matt@tecnovelty.com>
2021-01-12 21:07:06 -0800

    minor cleanup

--- msdk_apt/run.sh
+++ msdk_apt/run.sh
@@ -5,7 +5,7 @@
 # $ wget https://download.blender.org/peach/bigbuckbunny_movies/big_buck_bunny_1080p_h264.mov
 # $ ffmpeg -i big_buck_bunny_1080p_h264.mov -an -vcodec copy -bsf h264_mp4toannexb -f h264 big_buck_bunny_1080p_h264.264
 
-docker build -t msdk-apt .
-docker run -it --rm --privileged --net=host -e DISPLAY=$DISPLAY -v $HOME:/home msdk-apt /usr/share/mfx/samples/sample_decode h264 -i /home/big_buck_bunny_1080p_h264.264 -r -rgb4
+docker build -t msdk-apt:20.04 -f ubuntu_20.04/Dockerfile .
+docker run -it --rm --privileged --net=host -e DISPLAY=$DISPLAY -v $HOME:/home msdk-apt:20.04 /usr/share/mfx/samples/sample_decode h264 -i /home/big_buck_bunny_1080p_h264.264 -r -rgb4
 
 
